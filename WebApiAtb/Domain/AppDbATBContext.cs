314507caaf6b81167b775d9147e3a4bbbc3eaba2
novakvova <novakvova@gmail.com>
2025-09-12 16:07:08 +0300

    Add Domain

---
+++ WebApiAtb/Domain/AppDbATBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain;
+
+public class AppDbATBContext : DbContext
+{
+    public AppDbATBContext(DbContextOptions<AppDbATBContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Entities.CategoryEntity> Categories { get; set; } = null!;
+}
