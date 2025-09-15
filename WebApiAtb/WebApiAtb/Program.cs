9920d96f313c83c2a5f31146e7cc48d297d7aed1
novakvova <novakvova@gmail.com>
2025-09-15 17:05:08 +0300

    CRUD Categories Completed

--- WebApiAtb/WebApiAtb/Program.cs
+++ WebApiAtb/WebApiAtb/Program.cs
@@ -1,11 +1,19 @@
+using Core.Interfaces;
+using Core.Services;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbATBContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
@@ -20,5 +28,15 @@ app.UseSwaggerUI();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var dir = builder.Configuration["ImagesDir"];
+string path = Path.Combine(Directory.GetCurrentDirectory(), dir);
+Directory.CreateDirectory(path);
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(path),
+    RequestPath = $"/{dir}"
+});
 
 app.Run();
